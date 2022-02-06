2063ca74f3f5a6ad758be366007d3b1968586575
Fatema <fatemae110@gmail.com>
2022-02-06 17:34:16 +0530

    Added code for Union, Intersection, Difference, Symmetric Difference and Cartesian Product in set

--- src/main/scala/CS474/SetOperations.scala
+++ src/main/scala/CS474/SetOperations.scala
@@ -1,10 +1,12 @@
 package CS474
 
-import CS474.SetOperations.SetOper.Variable
+import CS474.SetOperations.SetOper.{Difference, Intersection, Variable}
+
+import scala.collection.mutable
 
 object SetOperations :
   type BasicType= Any
-  private var bindingScope: scala.collection.mutable.Map[String, BasicType] = collection.mutable.Map[String, BasicType]()
+  private var bindingScope: mutable.Map[String, BasicType] = collection.mutable.Map[String, BasicType]()
   enum SetOper:
     case Value(input: BasicType)
     case Variable(name: String)
@@ -12,22 +14,26 @@ object SetOperations :
     case Delete(setName: Variable, obj: SetOper)
     case Check(setName: Variable, obj:SetOper)
     case Union(setA:SetOper, setB:SetOper)
-//    private var setBindingScope: scala.collection.mutable.Map[Variable, BasicType] = collection.mutable.Map[Variable, BasicType]()
+    case Intersection(setA:SetOper, setB:SetOper)
+    case Difference(setA:SetOper, setB:SetOper)
+    case Symm_Difference(setA:SetOper, setB:SetOper)
+    case Cartesian_Product(setA:SetOper, setB:SetOper)
+//    private var setBindingScope: mutable.Map[Variable, BasicType] = collection.mutable.Map[Variable, BasicType]()
     def eval: BasicType = {
       this match {
         case Value(i) => i
         case Variable(name) => bindingScope.getOrElse(name,None)
 
-        case Insert(setname, objectList) =>
+        case Insert(setname, objectList) => println("Inserting")
           setname match{
             case Variable(name) => setname.eval match {
               case None =>
-                var setObj: scala.collection.mutable.Set[BasicType] = scala.collection.mutable.Set[BasicType]()
+                var setObj: mutable.Set[BasicType] = mutable.Set[BasicType]()
                 for (obj <- objectList) {
                   setObj += obj.eval
                 }
                 bindingScope(name) = setObj
-              case setObj: scala.collection.mutable.HashSet[BasicType] =>
+              case setObj: mutable.HashSet[BasicType] =>
                 for (obj <- objectList) {
                   setObj += obj.eval
                 }
@@ -41,7 +47,7 @@ object SetOperations :
           setname match{
             case Variable(name) => setname.eval match {
               case None => "No Such set variable defined"
-              case setObj: scala.collection.mutable.HashSet[BasicType] =>
+              case setObj: mutable.HashSet[BasicType] =>
                 setObj -= obj.eval
                 bindingScope(name) = setObj
               case _ => println("Not a set type")
@@ -53,7 +59,7 @@ object SetOperations :
           setname match {
             case Variable(name) => setname.eval match {
               case None => "No Such set variable defined"
-              case setObj: scala.collection.mutable.HashSet[BasicType] =>
+              case setObj: mutable.HashSet[BasicType] =>
                 setObj.contains(obj.eval)
               case _ => println("Not a set type")
             }
@@ -62,26 +68,72 @@ object SetOperations :
 
         case Union(setA, setB) => println("Union")
           setA.eval match {
-            case None => setB
-            case setObjA: scala.collection.mutable.HashSet[BasicType] =>
+            case None => setB.eval
+            case setObjA: mutable.HashSet[BasicType] =>
               setB.eval match {
-                case None => setA
-                case setObjB: scala.collection.mutable.HashSet[BasicType] =>
+                case None => setA.eval
+                case setObjB: mutable.HashSet[BasicType] =>
                   setObjA.union(setObjB)
                 case _ => println("B is not of Set type.")
               }
             case _ => println("A is not of Set type.")
           }
 
+        case Intersection(setA, setB) => println("Intersection")
+          setA.eval match {
+            case None => Set.empty
+            case setObjA: mutable.HashSet[BasicType] =>
+              setB.eval match {
+                case None => Set.empty
+                case setObjB: mutable.HashSet[BasicType] =>
+                  setObjA.intersect(setObjB)
+                case _ => println("B is not of Set type.")
+              }
+            case _ => println("A is not of Set type.")
+          }
+
+        case Difference(setA, setB) => println("Difference")
+          setA.eval match {
+            case None => setB.eval
+            case setObjA: mutable.HashSet[BasicType] =>
+              setB.eval match {
+                case None => setA.eval
+                case setObjB: mutable.HashSet[BasicType] =>
+                  setObjA.diff(setObjB)
+                case _ => println("B is not of Set type.")
+              }
+            case _ => println("A is not of Set type.")
+          }
+
+        case Symm_Difference(setA, setB) => println("Symmetric Difference")
+         val diff1 = Difference(setA, setB)
+         val diff2 = Difference(setB, setA)
+         Union(diff1,diff2).eval
+
+        case Cartesian_Product(setA, setB) => println("Cartesian Product")
+          setA.eval match {
+            case None => setB.eval
+            case setObjA: mutable.HashSet[BasicType] =>
+              setB.eval match {
+                case None => setA.eval
+                case setObjB: mutable.HashSet[BasicType] =>
+                  val cross = for {
+                    x <- setObjA; y <- setObjB
+                  } yield (x, y)
+                  cross
+                case _ => println("B is not of Set type.")
+              }
+            case _ => println("A is not of Set type.")
+          }
       }
 
     }
   @main def runSetExp: Unit =
     import SetOper.*
-    Insert(Variable("A"),List(Value("set"),Value("1234"), Value(1234))).eval
-    Insert(Variable("B"),List(Value("xyz"),Value("hello there"), Value(45))).eval
-    Delete(Variable("A"),Value(1234)).eval
+    Insert(Variable("A"),List(Value("set"),Value("34"), Value(1234))).eval
+    Insert(Variable("B"),List(Value("xyz"),Value("hello there"), Value("34"))).eval
+//    Delete(Variable("A"),Value(1234)).eval
     val thirdExpr = Check(Variable("A"), Value(1234)).eval
-//    var 4th = Union()
-    println(thirdExpr)
+    val D = Cartesian_Product(Variable("A"),Variable("C"))
+    println(D.eval)
 
