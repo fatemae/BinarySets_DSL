f181bdb10891a4b642530402cbf66799941491b9
Fatema <fatemae110@gmail.com>
2022-02-07 18:38:16 +0530

    Added code for Macro

--- build.sbt
+++ build.sbt
@@ -1,9 +1,18 @@
-ThisBuild / version := "0.1.0-SNAPSHOT"
+//ThisBuild / version := "0.1.0-SNAPSHOT"
+//
+//ThisBuild / scalaVersion := "3.1.1"
+//
+//lazy val root = (project in file("."))
+//  .settings(
+//    name := "Homework1",
+//    idePackagePrefix := Some("CS474")
+//  )
+version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "3.1.1"
+scalaVersion := "3.1.1"
+name := "Homework1"
+idePackagePrefix := Some("CS474")
+val ScalaTestVer = "3.2.9"
 
-lazy val root = (project in file("."))
-  .settings(
-    name := "Homework1",
-    idePackagePrefix := Some("CS474")
-  )
+libraryDependencies += "org.scalatest" %% "scalatest" % ScalaTestVer % Test
+libraryDependencies += "org.scalatest" %% "scalatest-featurespec" % ScalaTestVer % Test
--- src/main/scala/CS474/SetOperations.scala
+++ src/main/scala/CS474/SetOperations.scala
@@ -1,12 +1,13 @@
-package CS474
-
-import CS474.SetOperations.SetOper.{Difference, Intersection, Variable}
+package scala.CS474
 
 import scala.collection.mutable
 
 object SetOperations :
   type BasicType= Any
   private var bindingScope: mutable.Map[String, BasicType] = collection.mutable.Map[String, BasicType]()
+  private var macroMap: mutable.Map[String, SetOper] = collection.mutable.Map[String, SetOper]()
+  private var scopeMap: mutable.Map[String, Map[String, BasicType]] = collection.mutable.Map[String, Map[String, BasicType]]()
+
   enum SetOper:
     case Value(input: BasicType)
     case Variable(name: String)
@@ -18,7 +19,11 @@ object SetOperations :
     case Difference(setA:SetOper, setB:SetOper)
     case Symm_Difference(setA:SetOper, setB:SetOper)
     case Cartesian_Product(setA:SetOper, setB:SetOper)
-//    private var setBindingScope: mutable.Map[Variable, BasicType] = collection.mutable.Map[Variable, BasicType]()
+    case Macro(mName: String, operation: SetOper)
+    case MacroEval(mName: String)
+    case Scope(sName: String, oper:SetOper)
+
+
     def eval: BasicType = {
       this match {
         case Value(i) => i
@@ -33,13 +38,13 @@ object SetOperations :
                   setObj += obj.eval
                 }
                 bindingScope(name) = setObj
-              case setObj: mutable.HashSet[BasicType] =>
+              case setObj: mutable.Set[BasicType] =>
                 for (obj <- objectList) {
                   setObj += obj.eval
                 }
-              case _ => println("Not a set type")
+              case null => println("Not a set type")
             }
-            case _ => println("Setname not of variable type")
+            case null => println("Setname not of variable type")
           }
 
         case Delete(setname: Variable, obj: SetOper) =>
@@ -47,32 +52,32 @@ object SetOperations :
           setname match{
             case Variable(name) => setname.eval match {
               case None => "No Such set variable defined"
-              case setObj: mutable.HashSet[BasicType] =>
+              case setObj: mutable.Set[BasicType] =>
                 setObj -= obj.eval
                 bindingScope(name) = setObj
-              case _ => println("Not a set type")
+              case null => println("Not a set type")
             }
-            case _ => println("Setname not of variable type")
+            case null => println("Setname not of variable type")
           }
 
         case Check(setname, obj) => println("Check")
           setname match {
             case Variable(name) => setname.eval match {
               case None => "No Such set variable defined"
-              case setObj: mutable.HashSet[BasicType] =>
+              case setObj: mutable.Set[BasicType] =>
                 setObj.contains(obj.eval)
-              case _ => println("Not a set type")
+              case null => println("Not a set type")
             }
-            case _ => println("Setname not of variable type")
+            case null => println("Setname not of variable type")
           }
 
         case Union(setA, setB) => println("Union")
           setA.eval match {
             case None => setB.eval
-            case setObjA: mutable.HashSet[BasicType] =>
+            case setObjA: mutable.Set[BasicType] =>
               setB.eval match {
                 case None => setA.eval
-                case setObjB: mutable.HashSet[BasicType] =>
+                case setObjB: mutable.Set[BasicType] =>
                   setObjA.union(setObjB)
                 case _ => println("B is not of Set type.")
               }
@@ -82,10 +87,10 @@ object SetOperations :
         case Intersection(setA, setB) => println("Intersection")
           setA.eval match {
             case None => Set.empty
-            case setObjA: mutable.HashSet[BasicType] =>
+            case setObjA: mutable.Set[BasicType] =>
               setB.eval match {
                 case None => Set.empty
-                case setObjB: mutable.HashSet[BasicType] =>
+                case setObjB: mutable.Set[BasicType] =>
                   setObjA.intersect(setObjB)
                 case _ => println("B is not of Set type.")
               }
@@ -95,10 +100,10 @@ object SetOperations :
         case Difference(setA, setB) => println("Difference")
           setA.eval match {
             case None => setB.eval
-            case setObjA: mutable.HashSet[BasicType] =>
+            case setObjA: mutable.Set[BasicType] =>
               setB.eval match {
                 case None => setA.eval
-                case setObjB: mutable.HashSet[BasicType] =>
+                case setObjB: mutable.Set[BasicType] =>
                   setObjA.diff(setObjB)
                 case _ => println("B is not of Set type.")
               }
@@ -113,27 +118,28 @@ object SetOperations :
         case Cartesian_Product(setA, setB) => println("Cartesian Product")
           setA.eval match {
             case None => setB.eval
-            case setObjA: mutable.HashSet[BasicType] =>
+            case setObjA: mutable.Set[BasicType] =>
               setB.eval match {
                 case None => setA.eval
-                case setObjB: mutable.HashSet[BasicType] =>
+                case setObjB: mutable.Set[BasicType] =>
                   val cross = for {
                     x <- setObjA; y <- setObjB
                   } yield (x, y)
                   cross
-                case _ => println("B is not of Set type.")
+                case null => println("B is not of Set type.")
               }
-            case _ => println("A is not of Set type.")
+            case null => println("A is not of Set type.")
           }
+
+        case Macro(name, operation) => println("Macro")
+          macroMap(name) = operation
+
+        case MacroEval(name) => println("Substituting Macro")
+          val x :SetOper = macroMap.getOrElse(name, null)
+          if x!=null then
+            x.eval
+          else x
       }
 
     }
-  @main def runSetExp: Unit =
-    import SetOper.*
-    Insert(Variable("A"),List(Value("set"),Value("34"), Value(1234))).eval
-    Insert(Variable("B"),List(Value("xyz"),Value("hello there"), Value("34"))).eval
-//    Delete(Variable("A"),Value(1234)).eval
-    val thirdExpr = Check(Variable("A"), Value(1234)).eval
-    val D = Cartesian_Product(Variable("A"),Variable("C"))
-    println(D.eval)
-
+  
